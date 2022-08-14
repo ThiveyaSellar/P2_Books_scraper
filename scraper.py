8e9ebfd3f26613fccbe0aa831c02f0f2ba754fe6
Thiveya Sellar <sellarthiveya@gmail.com>
2022-08-14 20:41:00 +0200

    Téléchargement et sauvegarde des images découpés en sous fonctions

--- scraper.py
+++ scraper.py
@@ -1,10 +1,10 @@
-import requests
 from bs4 import BeautifulSoup
-import re
+import concurrent.futures
+import requests
 import pprint
 import csv
-import concurrent.futures
 import os
+import re
 
 
 # Retrait du signe "£"
@@ -13,12 +13,12 @@ def transform_price(price):
 
 
 # Récupérer la quantité en stock
-def retrieve_number_in_stock(a):
+def extract_number_in_stock(a):
     return int(re.search(r'\d+', a).group())
 
 
 # Récupérer la note sur 5
-def retrieve_rating(a):
+def extract_rating(a):
     rating = a['class'][1].upper()
     if rating == 'ONE':
         return 1
@@ -36,19 +36,16 @@ def retrieve_rating(a):
 
 # Récupérer diverses informations d'un produit sur sa page
 def retrieve_product_info(url):
-    # Récupérer les informations d'un produit
-
-    # Récupérer le contenu de la page grâce au package request
     page = requests.get(url)
-    if not page.ok :
+    if not page.ok:
         print("Problème chargement, code http : " + str(page.status_code))
-    # Parser le contenu récupéré graĉe au package BeautifulSoup
     soup = BeautifulSoup(page.content, 'html.parser')
+    
     upc = soup.find_all("td")[0].string
     title = soup.find("h1").string
     price_including_tax = transform_price(soup.find_all("td")[2].string)
     price_excluding_tax = transform_price(soup.find_all("td")[3].string)
-    number_available = retrieve_number_in_stock(soup.find_all("td")[5].string)
+    number_available = extract_number_in_stock(soup.find_all("td")[5].string)
     # Certains produits n'ont pas de description
     description = None
     try :
@@ -57,7 +54,7 @@ def retrieve_product_info(url):
         print(error)
         print("Pas description pour : \n" + url)
     category = soup.find("ul", class_="breadcrumb").find_all("a")[-1].text
-    review_rating = retrieve_rating(soup.find("p", class_="star-rating"))
+    review_rating = extract_rating(soup.find("p", class_="star-rating"))
 
     image_url = soup.find("div", id="product_gallery").find("img")['src']
     image_url = "https://books.toscrape.com/" + "/".join(image_url.split("/")[2:])
@@ -75,24 +72,27 @@ def retrieve_product_info(url):
         "image_url": image_url
     }
 
-    """print("Données récupérées : \n")
+    """
+    #Afficher proprement product
+    print("Données récupérées : \n")
     pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(product)"""
+    pp.pprint(product)
+    """
     return product
 
 
 # Sauvegarder les informations extraites d'un produit dans un fichier csv
-def save_product_info(product) :
-    #En-tête du fichier CSV avec les clés du dictionnaire
+def save_product_info(product):
+    # En-tête du fichier CSV avec les clés du dictionnaire
     header = []
     for key in product.keys():
         header.append(key)
-
+    # Définir le nom du fichier à partir du titre du livre
     name = product['title']
-    name = name.split(' ')
-    name = '_'.join(name)
+    name = '_'.join(name.split(' '))
     file_name = name + '_info.csv'
-    with open(file_name,'w') as csv_file:
+    # Ouvrir et écrire dans le fichier csv
+    with open(file_name, 'w') as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
         writer.writerow(header)
         line = []
@@ -104,38 +104,37 @@ def save_product_info(product) :
 # Extraire les url des produits sur une page
 def extract_page_product_url(soup, tab, url_racine):
     product_url_list = soup.find_all("h3")
-    for a in product_url_list:
-        b = url_racine + '/' +a.find("a")['href']
-        tab.append(b)
+    for e in product_url_list:
+        product_url = url_racine + '/' + e.find("a")['href']
+        tab.append(product_url)
 
 
 # Extraire les urls des produits sur toutes les pages d'une catégorie
 def extract_all_product_url(url, category):
-
     # Requête avec le lien de la première page de la catégorie
     page = requests.get(url)
-    soup = BeautifulSoup(page.content,'html.parser')
+    soup = BeautifulSoup(page.content, 'html.parser')
 
     # Tableau des urls de la catégorie
     category_product_url = []
     # Récupérer les liens de tous les produits de la page actuelle
-    urla = url.split("/")
-    urla = urla[0:-1]
-    u = "/".join(urla)
+    u = url.split("/")
+    u = u[0:-1]
+    u = "/".join(u)
     extract_page_product_url(soup, category_product_url, u)
-    # Chercher l'url de la page suivante
+    # Chercher url de la page suivante
     next_page = soup.find("li", class_="next")
 
     # Tant qu'il existe une page suivante
     # Récupérer les url de tous les produits
     # Vérifier qu'il existe une page suivante
     # Récupérer la page
-    while next_page != None :
+    while next_page is not None:
         next_url = next_page.find("a")
         url = u + '/' + next_url['href']
         #print("http://books.toscrape.com/" + url)
         page = requests.get(url)
-        soup = BeautifulSoup(page.content,'html.parser')
+        soup = BeautifulSoup(page.content, 'html.parser')
         extract_page_product_url(soup, category_product_url, u)
         next_page = soup.find("li", class_="next")
     return category_product_url
@@ -143,21 +142,17 @@ def extract_all_product_url(url, category):
 
 # Extraire les urls des produits d'une catégorie
 def find_category_url(category):
-
-    # Définir une expression régulière pour extraire l'url contenant la catégorie
+    # Définir une expression régulière pour extraire url contenant la catégorie
     reg_exp = "./" + category + "_."
-    # Chercher l'url contenant la catégorie souhaitée
+    # Chercher url contenant la catégorie souhaitée
     category_url = soup.find("a", href=re.compile(reg_exp))
-
-
-
-    # Définir l'url complet de la catégorie
+    # Définir url complet de la catégorie
     category_url = "http://books.toscrape.com/" + category_url['href']
     return category_url
 
 
 def save_category_product_info(urls, file_name, directory):
-    #En-tête du fichier CSV
+    # En-tête du fichier CSV
     header = [
         "product_page_url",
         "universal_product_code",
@@ -170,19 +165,18 @@ def save_category_product_info(urls, file_name, directory):
         "review_rating",
         "image_url"
     ]
-
-    with open(directory + "/" + file_name,'w') as csv_file:
+    with open(directory + "/" + file_name, 'w') as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
         writer.writerow(header)
-
         for url in urls:
             product = retrieve_product_info(url)
             line = []
             for k, v in product.items():
                 line.append(v)
             writer.writerow(line)
+    print("Fichier créé : " + file_name)
 
-def choose_category() :
+def choose_category():
     # Récupérer le bloc des catégories
     category_section = soup.find("div", class_="side_categories")
     # Lister tous les url des catégories
@@ -194,32 +188,32 @@ def choose_category() :
         c = category.text.strip()
         print(c)
         categories.append(c.lower())
-
     # Demander de choisir une catégorie
     # Si catégorie est absente de la liste des catégories, répéter la saisie
-    category = input("\nChoose a category : ").lower()
+    category = input("\nChoisir une catégorie : ").lower()
     while category not in categories:
-        category = input("\nPlease write a valid category : ").lower()
-
+        category = input("\nChoisir une catégorie valide : ").lower()
     return category
 
+
 def scrap_category(category, directory):
-    print(category)
+    print("\n" + category.upper())
     c_url = find_category_url(category)
     print(c_url)
-    c_url_list = extract_all_product_url(c_url,category)
+    c_url_list = extract_all_product_url(c_url, category)
     save_category_product_info(c_url_list, category + "_product_info.csv", directory)
     return len(c_url_list)
 
-def extract_images_urls(soup, tab_img, tab_title):
+
+def extract_page_images_urls(soup, tab_img, tab_title):
     img_tags = soup.findAll("img")
-    h3_tags = soup.findAll("h3") #.find("h3").find("a")['title']
-    for h3 in h3_tags :
+    h3_tags = soup.findAll("h3")
+    for h3 in h3_tags:
         title = h3.find("a")['title']
         title = title.replace("/", "_")
-        title = ("_").join(title.split(' '))
+        title = "_".join(title.split(' '))
         tab_title.append(title)
-    for tag in img_tags :
+    for tag in img_tags:
         image_url = tag['src']
         image_url = image_url.split("/")
         image_url = image_url[3:]
@@ -228,37 +222,67 @@ def extract_images_urls(soup, tab_img, tab_title):
         tab_img.append(image_url)
 
 
+def extract_all_images_urls(soup, tab_urls, tab_titles):
+    extract_page_images_urls(soup, tab_urls, tab_titles)
+    next_page = soup.find("li", class_="next")
+    while next_page is not None:
+        next_url = "https://books.toscrape.com/catalogue/category/books_1/" + next_page.find("a")['href']
+        page = requests.get(next_url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        extract_page_images_urls(soup, tab_urls, tab_titles)
+        next_page = soup.find("li", class_="next")
+
+
+def save_products_images(tab_urls, tab_titles, dir):
+    for i in range(len(tab_urls)):
+        file = open(dir + '/' + tab_titles[i] + '.jpg', 'wb')
+        response = requests.get(tab_urls[i])
+        file.write(response.content)
+        file.close()
+    print("Les images des produits sont enregistrées : " + dir + "/")
+
+
 # ----------------------------------------------------------------------------------------
 
-    # Récupérer les informations d'un produit à partir de l'url de sa page
+    # Récupérer les informations d'un produit à partir url de sa page
 
 # ----------------------------------------------------------------------------------------
+print("\n1) Extraire les informations d'un produit à partir de l'url de sa page")
+print("Souhaitez-vous saisir l'url d'un produit ? 'O' pour oui ou 'N' non")
+rep = input().lower()
+while rep != 'o' and rep != 'n':
+    print("Saisir 'O' pour oui ou 'N' non")
+    rep = input().lower()
+if rep == 'o':
+    print("Saisir l'url d'un produit")
+    url_choisi = input()
+    product = retrieve_product_info(url_choisi)
+    print("Données récupérées : \n")
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(product)
+    save_product_info(product)
+else:
+    url_ex = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
+    product = retrieve_product_info(url_ex)
+    print("Données récupérées : \n")
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(product)
+    save_product_info(product)
 
-"""url = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
-product = retrieve_product_info(url)
-save_product_info(product)
-
-"""
 # ----------------------------------------------------------------------------------------
 
     # Extraire les url et les informations des produits d'une catégorie
 
 # ----------------------------------------------------------------------------------------
-
-# Requête avec url de la page principale
 URL_MAIN = "http://books.toscrape.com/index.html"
 page = requests.get(URL_MAIN)
 soup = BeautifulSoup(page.content, 'html.parser')
-
-
-# Afficher la liste des catégories et choisir une catégorie
+print("\n2) Extraire les informations des produits d'une catégorie")
 category = choose_category()
-# Récupérer l'url de la catégorie
+category = category.replace(" ", "-")
 category_url = find_category_url(category)
 # Récupérer la liste des urls de tous les produits de la catégorie souhaitée
-url_list = extract_all_product_url(category_url,category)
-
-# Récupérer les informations de tous les produits et les enregistrer dans un fichier csv
+url_list = extract_all_product_url(category_url, category)
 save_category_product_info(url_list, category + "_product_info.csv", "./")
 
 # ----------------------------------------------------------------------------------------
@@ -266,16 +290,15 @@ save_category_product_info(url_list, category + "_product_info.csv", "./")
     # Extraire toutes les catégories
 
 # ----------------------------------------------------------------------------------------
-
-# Extraire toutes les catégories
+print("\n3) Extraire les informations des produits de toutes les catégories")
 category_section = soup.find("div", class_="side_categories")
 category_url_list = category_section.find_all("a")[1:]
 categories = []
 # Afficher les catégories et les ajouter à la liste des catégories
 for category in category_url_list:
     c = category.text.strip()
-    if " " in c :
-        c = c.replace(' ','-')
+    if " " in c:
+        c = c.replace(' ', '-')
     categories.append(c.lower())
 
 category_dir = "Categories"
@@ -288,39 +311,30 @@ except FileExistsError as error:
 
 with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
     a = []
-    for c in categories :
-        a.append(executor.submit(scrap_category,c,category_dir))
+    for c in categories:
+        a.append(executor.submit(scrap_category, c, category_dir))
     executor.shutdown()
-    print(sum([ f.result() for f in a ]))
+    print("Nombre de produits scrappés : " + sum([f.result() for f in a]))
 
 # ----------------------------------------------------------------------------------------
 
-    # Télécharger les images des pages produits visitées
+    # Télécharger les images des pages produits
 
 # ----------------------------------------------------------------------------------------
-
+print("\n4) Télécharger et enregistrer les images des produits")
 page = requests.get("https://books.toscrape.com/catalogue/category/books_1/index.html")
 soup = BeautifulSoup(page.content, 'html.parser')
 tab_img_urls = []
 tab_img_titles = []
-extract_images_urls(soup,tab_img_urls,tab_img_titles)
-next_page = soup.find("li", class_="next")
-while next_page != None :
-    next_url = "https://books.toscrape.com/catalogue/category/books_1/" + next_page.find("a")['href']
-    page = requests.get(next_url)
-    soup = BeautifulSoup(page.content,'html.parser')
-    extract_images_urls(soup, tab_img_urls, tab_img_titles)
-    next_page = soup.find("li",class_="next")
+
+extract_all_images_urls(soup, tab_img_urls,tab_img_titles)
+
 directory = "Products_images"
 parent_directory = "./"
 path = os.path.join(parent_directory, directory)
-try :
+try:
     os.mkdir(path)
-except FileExistsError as error :
+except FileExistsError as error:
     print(error)
 
-for i in range(len(tab_img_urls)):
-    file = open('Products_images/' + tab_img_titles[i] + '.jpg', 'wb')
-    response = requests.get(tab_img_urls[i])
-    file.write(response.content)
-    file.close()
+save_products_images(tab_img_urls, tab_img_titles,directory)
